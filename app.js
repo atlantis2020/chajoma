786ff22f83084891658de4c9337dd77eb838ea0f
Marek L <nospam.keram@gmail.com>
2015-09-12 17:13:49 +0100

    backup

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var server = app.listen(3001, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
