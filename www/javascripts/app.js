2f0313c3c86dd0fe897b546fe9e8255c8840b5da
Marek L <nospam.keram@gmail.com>
2015-09-12 22:15:05 +0100

    render nepal earthquake data to map

--- www/javascripts/app.js
+++ www/javascripts/app.js
@@ -2,11 +2,11 @@
 
 // import io from 'socket.io-client';
 
-(function () {
+(function (L) {
     'use strict';
 
     var doc = $(document),
-        drawers;
+        featureRenderer;
 
     // var doc = $(document),
     //     socket = io('http://localhost:3001');
@@ -24,33 +24,42 @@
     //     console.log('-t-', position.coords.latitude, position.coords.longitude);
     // });
 
+    function reverseCoordinates(point) {
+        return [point[1], point[0]];
+    }
 
-    draw = {
-        drawMultiLineString: function (feature) {
-            console.log(feature);
+    featureRenderer = {
+        drawMultiLineString: function (map, feature) {
+            var coordinates = feature.geometry.coordinates.map(
+                function(line) {
+                    return line.map(reverseCoordinates);
+                });
+
+            L.polygon(coordinates, feature.properties).addTo(map);
+        },
+        drawCircle: function(map, feature) {
+            L.circle([51.508, -0.11], 50, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5
+            }).addTo(map);
         }
     }
 
 
     function initMap() {
-        // var map = L.map('map').setView([51.505, -0.09], 13),
         var map = L.map('map');
+
+        var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib = '© <a href="http://openstreetmap.org">OpenStreetMap</a>';
+        var osm = new L.TileLayer(osmUrl, {
+            minZoom: 5, maxZoom: 16, attribution: osmAttrib
+        });
+
+        map.addLayer(osm);
 
         return map;
 
-        // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
-        //     maxZoom: 18,
-        //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        //         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        //         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        //     id: 'mapbox.streets'
-        // }).addTo(map);
-
-        // L.circle([51.508, -0.11], 50, {
-        //     color: 'red',
-        //     fillColor: '#f03',
-        //     fillOpacity: 0.5
-        // }).addTo(map);
     }
 
     function computeBoxCenter(coords) {
@@ -61,16 +70,21 @@
         return $.getJSON('/www/data/nepal-shakemap/cont-mi.json')
             .then(function (response) {
                 var center = computeBoxCenter(response.bbox);
-                map.setView(new L.LatLng(center[0], center[1]), 8);
+                map.setView(
+                    new L.LatLng(
+                        response.metadata.latitude,
+                        response.metadata.longitude
+                    ), 7);
                 drawFeatures(map, response.features);
-                // map.setView(new L.LatLng(84.8645, 28.19270), 5);
             });
     }
 
     function drawFeatures(map, features) {
-        console.log(features, features.length, 't-t-');
+        var fr = featureRenderer;
 
         features.map(function (feature) {
+            fr['draw' + feature.geometry.type](map, feature);
+        });
 
         return map;
     }
@@ -78,15 +92,6 @@
     $(function () {
         var map = initMap();
 
-
-        var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new L.TileLayer(osmUrl, {
-            minZoom: 5, maxZoom: 12, attribution: osmAttrib
-        });
-
-        map.addLayer(osm);
-
         getData(map);
     });
-}());
+}(L));
